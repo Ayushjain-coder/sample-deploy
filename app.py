d62383f5f3c94d7bf4642b8dd1d9ffbce36c5144
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 00:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask
+import eel
 
 app = Flask(__name__)
 
 @app.route('/')
 def run_script():
-    file = open(r'/main.py', 'r').read()
-    return exec(file)
+    eel.init(r'\web', allowed_extensions=['.js', '.html'])
+    try:
+        eel.start('index.html', mode='chrome',
+                                host='localhost',
+                                port=27005,
+                                block=False,
+                                size=(350, 480),
+                                position=(10,100),
+                                disable_cache=True,
+                                close_callback=ChatBot.close_callback)
+        ChatBot.started = True
+        while ChatBot.started:
+            try:
+                eel.sleep(10.0)
+            except:
+                #main thread exited
+                break
+
+    except:
+        pass
 
 if __name__ == "__main__":
     app.run(debug=True)
