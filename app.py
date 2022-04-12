e8c5aada86766aa284ff427c8dcbb5f2b0791266
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 01:16:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,61 @@
-import eel
-# import os
-from queue import Queue
+# import eel
+# # import os
+# from queue import Queue
 
-class ChatBot:
+# class ChatBot:
 
-    started = False
-    userinputQueue = Queue()
+#     started = False
+#     userinputQueue = Queue()
 
-    def isUserInput():
-        return not ChatBot.userinputQueue.empty()
+#     def isUserInput():
+#         return not ChatBot.userinputQueue.empty()
 
-    def popUserInput():
-        return ChatBot.userinputQueue.get()
+#     def popUserInput():
+#         return ChatBot.userinputQueue.get()
 
-    def close_callback(route, websockets):
-        # if not websockets:
-        #     print('Bye!')
-        exit()
+#     def close_callback(route, websockets):
+#         # if not websockets:
+#         #     print('Bye!')
+#         exit()
 
-    @eel.expose
-    def getUserInput(msg):
-        ChatBot.userinputQueue.put(msg)
-        print(msg)
+#     @eel.expose
+#     def getUserInput(msg):
+#         ChatBot.userinputQueue.put(msg)
+#         print(msg)
     
-    def close():
-        ChatBot.started = False
+#     def close():
+#         ChatBot.started = False
     
-    def addUserMsg(msg):
-        eel.addUserMsg(msg)
+#     def addUserMsg(msg):
+#         eel.addUserMsg(msg)
     
-    def addAppMsg(msg):
-        eel.addAppMsg(msg)
+#     def addAppMsg(msg):
+#         eel.addAppMsg(msg)
 
-    def start():
-#         path = os.path.dirname(os.path.abspath(__file__))
-        eel.init(path + r'\web', allowed_extensions=['.js', '.html'])
-        try:
-            eel.start('index.html', mode='chrome',
-                                    host='localhost',
-                                    port=27005,
-                                    block=False,
-                                    size=(350, 480),
-                                    position=(10,100),
-                                    disable_cache=True,
-                                    close_callback=ChatBot.close_callback)
-            ChatBot.started = True
-            while ChatBot.started:
-                try:
-                    eel.sleep(10.0)
-                except:
-                    #main thread exited
-                    break
+#     def start():
+# #         path = os.path.dirname(os.path.abspath(__file__))
+#         eel.init(path + r'\web', allowed_extensions=['.js', '.html'])
+#         try:
+#             eel.start('index.html', mode='chrome',
+#                                     host='localhost',
+#                                     port=27005,
+#                                     block=False,
+#                                     size=(350, 480),
+#                                     position=(10,100),
+#                                     disable_cache=True,
+#                                     close_callback=ChatBot.close_callback)
+#             ChatBot.started = True
+#             while ChatBot.started:
+#                 try:
+#                     eel.sleep(10.0)
+#                 except:
+#                     #main thread exited
+#                     break
         
-        except:
-            pass
+#         except:
+#             pass
 
-obj = ChatBot()
-obj.start()
+# obj = ChatBot()
+# obj.start()
+
+print("hello")
