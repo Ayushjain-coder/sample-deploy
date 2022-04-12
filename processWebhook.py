5d63e8d3290b7c1a08a25b20c64769e807c86606
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-12 22:38:20 +0530

    Update processWebhook.py

--- processWebhook.py
+++ processWebhook.py
@@ -1,4 +1,6 @@
-import streamlit as st
+import streamlit as st 
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 html_temp = """
    <div class="" style="background-color:green;" >
@@ -12,7 +14,18 @@ html_temp = """
    </div>
    """
 st.markdown(html_temp,unsafe_allow_html=True)
+st.header("NLP Review System ")
+  
+  
+text = st.text_area("Write Your Reviews")
 
+if st.button("Review Analysis"):
+  result=review(text)
+  st.success('Model has predicted {}'.format(result))
+      
+if st.button("About"):
+  st.subheader("Ayush Jain, PIET Jaipur")
+  
 html_temp = """
    <div class="" style="background-color:blue;" >
    <div class="clearfix">           
