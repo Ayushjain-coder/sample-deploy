361f9bc1ec1ea7e1490ba344f163968d70d16f6f
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-12 23:05:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,10 @@
-import streamlit as st 
+from flask import Flask
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+app = Flask(__name__)
 
-html_temp = """
-   <div class="" style="background-color:green;" >
-   <div class="clearfix">           
-   <div class="col-md-12">
-   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
-   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:white;margin-top:10px;"Machine Learning Lab II Midterm Examination</p></center> 
-   </div>
-   </div>
-   </div>
-   """
-st.markdown(html_temp,unsafe_allow_html=True)
-st.header("NLP Review System ")
-  
-  
-text = st.text_area("Write Your Reviews")
+@app.route('/')
+def hello_world():
+   return 'Hello World!'
 
-if st.button("Review Analysis"):
-  result=review(text)
-  st.success('Model has predicted {}'.format(result))
-      
-if st.button("About"):
-  st.subheader("Ayush Jain, PIET Jaipur")
-  
-html_temp = """
-   <div class="" style="background-color:blue;" >
-   <div class="clearfix">           
-   <div class="col-md-12">
-   <center><p style="font-size:20px;color:white;margin-top:10px;">Machine learning Midterm Experiment NLP</p></center> 
-   </div>
-   </div>
-   </div>
-   """
-st.markdown(html_temp,unsafe_allow_html=True)
+if __name__ == '__main__':
+   app.run()
