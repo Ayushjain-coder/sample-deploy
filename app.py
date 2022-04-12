6054acbccf2ea63f2f264a250836b60de4b70cf8
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-12 23:55:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
-from flask import Flask, render_template
+import eel
+import os
+from queue import Queue
 
-app = Flask(__name__)
+class ChatBot:
 
-@app.route('/')
-def main():
-    file = open(r'/app.py', 'r').read()
-    return exec(file)
+    started = False
+    userinputQueue = Queue()
 
-if __name__ == '__main__':
-   app.run()
+    def isUserInput():
+        return not ChatBot.userinputQueue.empty()
+
+    def popUserInput():
+        return ChatBot.userinputQueue.get()
+
+    def close_callback(route, websockets):
+        # if not websockets:
+        #     print('Bye!')
+        exit()
+
+    @eel.expose
+    def getUserInput(msg):
+        ChatBot.userinputQueue.put(msg)
+        print(msg)
+    
+    def close():
+        ChatBot.started = False
+    
+    def addUserMsg(msg):
+        eel.addUserMsg(msg)
+    
+    def addAppMsg(msg):
+        eel.addAppMsg(msg)
+
+    def start():
+        path = os.path.dirname(os.path.abspath(__file__))
+        eel.init(path + r'\web', allowed_extensions=['.js', '.html'])
+        try:
+            eel.start('index.html', mode='chrome',
+                                    host='localhost',
+                                    port=27005,
+                                    block=False,
+                                    size=(350, 480),
+                                    position=(10,100),
+                                    disable_cache=True,
+                                    close_callback=ChatBot.close_callback)
+            ChatBot.started = True
+            while ChatBot.started:
+                try:
+                    eel.sleep(10.0)
+                except:
+                    #main thread exited
+                    break
+        
+        except:
+            pass
