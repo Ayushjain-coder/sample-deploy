566e321dd307527d440616650bdf37eaeea71b90
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 00:13:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,13 @@
-from flask import Flask
-import eel
+from flask import Flask, render_template
+from flaskwebgui import FlaskUI
 
 app = Flask(__name__)
 
-@app.route('/')
-def run_script():
-    eel.init(r'\web', allowed_extensions=['.js', '.html'])
-    try:
-        eel.start('index.html', mode='chrome',
-                                host='localhost',
-                                port=27005,
-                                block=False,
-                                size=(350, 480),
-                                position=(10,100),
-                                disable_cache=True,
-                                close_callback=ChatBot.close_callback)
-        ChatBot.started = True
-        while ChatBot.started:
-            try:
-                eel.sleep(10.0)
-            except:
-                #main thread exited
-                break
+ui = FlaskUI(app, width=350, height=480)
 
-    except:
-        pass
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    ui.run(debug=True)
