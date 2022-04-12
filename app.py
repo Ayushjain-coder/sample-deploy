983521792e60bb17238bf9ecedea7ed92750b156
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 00:20:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ ui = FlaskUI(app, width=350, height=480)
 
 @app.route('/')
 def main():
-    return render_template('web/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     ui.run(debug=True)
