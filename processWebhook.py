a9b5e2814eabd5181a45ba8315c7d9f97b0a68ce
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-12 22:26:34 +0530

    Update processWebhook.py

--- processWebhook.py
+++ processWebhook.py
@@ -1,20 +1,25 @@
-import flask
-import os
-from flask import send_from_directory
+import streamlit as st
 
-app = flask.Flask(__name__)
+html_temp = """
+   <div class="" style="background-color:green;" >
+   <div class="clearfix">           
+   <div class="col-md-12">
+   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
+   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
+   <center><p style="font-size:25px;color:white;margin-top:10px;"Machine Learning Lab II Midterm Examination</p></center> 
+   </div>
+   </div>
+   </div>
+   """
+st.markdown(html_temp,unsafe_allow_html=True)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return "Hello World"
-
-if __name__ == "__main__":
-    app.secret_key = 'ItIsASecret'
-    app.debug = True
-    app.run()
+html_temp = """
+   <div class="" style="background-color:blue;" >
+   <div class="clearfix">           
+   <div class="col-md-12">
+   <center><p style="font-size:20px;color:white;margin-top:10px;">Machine learning Midterm Experiment NLP</p></center> 
+   </div>
+   </div>
+   </div>
+   """
+st.markdown(html_temp,unsafe_allow_html=True)
