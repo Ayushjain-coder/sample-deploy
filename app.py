ae9f2712de4e88b462ca8e1598f1e1643b205f03
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 16:16:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template, Response
-from camera import VideoCamera
+import cv2
+import streamlit as st
 
-app = Flask(__name__)
+st.title("Webcam Application")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+cam = cv2.VideoCapture(1)
 
-@app.route('/')
-def index():
-    return render_template('index1.html')
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+while run:
+    ret, frame = cam.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
