23e797955be8cdcf76a00a120da849cb34d8d6b9
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 16:23:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ cam = cv2.VideoCapture(1)
 
 while run:
     ret, frame = cam.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
