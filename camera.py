404dda0e5b87b2b76b70e4de761495dce0270130
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 15:07:23 +0530

    Add files via upload

--- camera.py
+++ camera.py
@@ -2,12 +2,12 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-       self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture(0)
     
     def __del__(self):
         self.video.release()
-        
-def get_frame(self):
-        success, frame = self.video.read()
-        ret, jpeg = cv2.imencode('.jpg', frame)
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
