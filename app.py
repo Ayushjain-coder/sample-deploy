a6b89db159ea41683a42b2ff216ae32dba5af76a
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 16:44:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-import cv2
-import streamlit as st
+from flask import Flask, render_template, Response
+from camera import VideoCamera
 
-st.title("Webcam Application")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-cam = cv2.VideoCapture(0)
+app = Flask(__name__)
 
-while run:
-    ret, frame = cam.read()
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+@app.route('/')
+def index():
+    return render_template('index1.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
