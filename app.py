108cb3f6cc08b860502bae27bbb5ea58bd2d7dc6
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 10:32:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from flaskwebgui import FlaskUI
 
 app = Flask(__name__)
+
+ui = FlaskUI(app, width=350, height=480)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    ui.run(debug=True)
