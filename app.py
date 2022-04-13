8e58a567ba3246a348af073fa0adf9e2812547df
Ayush Jain <52110863+Ayushjain-coder@users.noreply.github.com>
2022-04-13 14:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response
 import cv2
-import os, sys
 
-global capture,rec_frame, grey, switch, neg, face, rec, out 
-capture=0
-grey=0
-neg=0
-face=0
-switch=1
-rec=0
+app = Flask(__name__)
 
-#instatiate flask app  
-app = Flask(__name__, template_folder='./templates')
-
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)  # use 0 for web camera
 
 def gen_frames():  # generate frame by frame from camera
-    global out, capture,rec_frame
     while True:
-        success, frame = camera.read() 
-        pass
+        # Capture frame-by-frame
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-@app.route('/')
-def index():
-    return render_template('index1.html')
 
 @app.route('/video_feed')
 def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-    app.run()
-    
-camera.release()
-cv2.destroyAllWindows()     
+    app.run(debug=True)
